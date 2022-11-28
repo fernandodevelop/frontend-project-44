f302740c8bd428525843c22284063bd8fb40eac5
AlexeyLeo <alexeyleo83@gmail.com>
2022-11-28 22:18:35 +0300

    step4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import {queryName} from '../src/cli.js';
+import queryName from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 queryName();
